48d694e1d48b312b8b64d29801de1ab317868756
maximkorskov01 <maximkorskov01@gmail.com>
2025-06-30 03:19:10 +0300

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,198 @@
+import json
+import aiosqlite
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram import F
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
+API_TOKEN = '7545897030:AAFCTWt5ckN_ztHuODSqfyepWwRsLApfowY'
+
+# Объект бота
+bot = Bot(token=API_TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+# Зададим имя базы данных
+DB_NAME = 'quiz_bot.db'
+
+
+# Загружаем вопросы из JSON файла
+with open('questions.json', 'r', encoding='utf-8') as file:
+    quiz_data = json.load(file)
+
+
+def generate_options_keyboard(answer_options, right_answer):
+    builder = InlineKeyboardBuilder()
+
+    for option in answer_options:
+        builder.add(types.InlineKeyboardButton(
+            text=option,
+            callback_data="right_answer" if option == right_answer else "wrong_answer")
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+@dp.callback_query(F.data == "right_answer")
+async def right_answer(callback: types.CallbackQuery):
+
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    await callback.message.answer("✅ Ваш ответ: Верно!")
+    current_question_index = await get_quiz_index(callback.from_user.id)
+    current_score = await get_user_score(callback.from_user.id)
+    print(f"Полученное значение current_score: {current_score}")  
+    if current_score is None:
+        current_score = 0 
+    current_score += 1
+    print(f"Обновленное значение current_score: {current_score}") 
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await update_quiz_index(callback.from_user.id, current_question_index)
+    await update_user_score(callback.from_user.id, current_score)
+
+
+    if current_question_index < len(quiz_data):
+        await get_question(callback.message, callback.from_user.id)
+    else:
+        await callback.message.answer(f"⭐️Квиз завершен, спасибо за участие!\n🏆 Ваш результат: {current_score} правильных ответов")
+
+
+@dp.callback_query(F.data == "wrong_answer")
+async def wrong_answer(callback: types.CallbackQuery):
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    # Получение текущего вопроса из словаря состояний пользователя
+    current_question_index = await get_quiz_index(callback.from_user.id)
+    current_score = await get_user_score(callback.from_user.id)
+    correct_option = quiz_data[current_question_index]['correct_option']
+
+    await callback.message.answer(f"❌ Ваш ответ: Неправильно.\n\n📚 Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
+
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await update_quiz_index(callback.from_user.id, current_question_index)
+    await update_user_score(callback.from_user.id, current_score)
+
+
+    if current_question_index < len(quiz_data):
+        await get_question(callback.message, callback.from_user.id)
+    else:
+        await callback.message.answer(f"⭐️Квиз завершен, спасибо за участие!\n\n🏆Ваш результат: {current_score} правильных ответов")
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.add(types.KeyboardButton(text="Начать игру"))
+    await message.answer("Добро пожаловать в квиз!", reply_markup=builder.as_markup(resize_keyboard=True))
+
+
+
+
+async def get_question(message, user_id):
+
+    # Получение текущего вопроса из словаря состояний пользователя
+    current_question_index = await get_quiz_index(user_id)
+    correct_index = quiz_data[current_question_index]['correct_option']
+    opts = quiz_data[current_question_index]['options']
+    kb = generate_options_keyboard(opts, opts[correct_index])
+    await message.answer(f"📝 Вопрос {current_question_index + 1}/{len(quiz_data)}\n\n"f"{quiz_data[current_question_index]['question']}", reply_markup=kb)
+
+async def new_quiz(message):
+    logging.info("Начался новый квиз")
+    user_id = message.from_user.id
+    current_question_index = 0
+    new_score = 0
+    await update_quiz_index(user_id, current_question_index)
+    await update_user_score(user_id, new_score)
+    await get_question(message, user_id)
+
+async def get_quiz_index(user_id):
+     # Подключаемся к базе данных
+     async with aiosqlite.connect(DB_NAME) as db:
+        # Получаем запись для заданного пользователя
+        async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
+            # Возвращаем результат
+            results = await cursor.fetchone()
+            if results is not None:
+                return results [0]
+            else:
+                return 0
+
+
+async def get_user_score(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute('SELECT score FROM users WHERE user_id = ?', (user_id,)) as cursor:
+            result = await cursor.fetchone()
+            if result is not None:
+                return result [0]
+            else:
+                return 0
+
+
+async def update_quiz_index(user_id, index):
+    # Создаем соединение с базой данных (если она не существует, она будет создана)
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Вставляем новую запись или заменяем ее, если с данным user_id уже существует
+        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
+        # Сохраняем изменения
+        await db.commit()
+
+
+async def update_user_score(user_id, new_score):
+    print(f"Обновление счёта для пользователя {user_id} до {new_score}")
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute('INSERT INTO users (user_id, score) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET score = excluded.score', (user_id, new_score))
+        await db.commit()
+
+
+@dp.message(F.text=="Начать игру")
+@dp.message(Command("quiz"))
+async def cmd_quiz(message: types.Message):
+    logging.info("Получен запрос на начало квиза")
+    await message.answer(f"Давайте начнем квиз!")
+    await new_quiz(message)
+
+
+
+
+async def create_table():
+    # Создаем соединение с базой данных (если она не существует, она будет создана)
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
+        await db.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, score INTEGER)''')
+        # Сохраняем изменения
+        await db.commit()
+
+@dp.message(Command('help'))
+async def cmd_help(message: types.Message):
+    await message.answer('Команды бота:\n/start - начать взаимодействие с ботом\n/help - открыть помощь\n/quiz - начать игру')
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+
+    # Запускаем создание таблицы базы данных
+    await create_table()
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
